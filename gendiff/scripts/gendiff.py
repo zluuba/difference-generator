b497afb70fc34f32e61a4d9613acccef54d2909d
zluuba <zluuba@MacBook-Pro-zluuba.local>
2022-12-20 16:24:56 +0300

    Feat: add format flag

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -8,8 +8,10 @@ parser = argparse.ArgumentParser(
 
 parser.add_argument('first_file')
 parser.add_argument('second_file')
+parser.add_argument('-f', '--format', help='set format of output')
 
 args = parser.parse_args()
+
 
 def main():
     print(args.accumulate(args.integers))
