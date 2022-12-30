aa5acd1949fbfe5ca64777321466334be1ecaf23
zluuba <zluyba.nikitina@gmail.com>
2022-12-30 17:24:18 +0300

    Fix: update stylish

--- gendiff/formatters/stylish.py
+++ gendiff/formatters/stylish.py
@@ -29,8 +29,8 @@ def get_format_(dictionary, replacer=' ', count=1, indent=3):
         for key, value in node.items():
             new_key = add_char_to_key(key)
 
-            line = f'{deep_indent}{new_key}: ' \
-                   f'{walker(value, deep_indent_size + indent)}'
+            line = f'{deep_indent}{new_key}:' \
+                   f' {walker(value, deep_indent_size + indent)}'
             lines.append(line.rstrip())
 
         result = itertools.chain("{", lines, [current_indent + "}"])
