1b0d235bf54b543ac251658abcbbf54984677694
zluuba <zluyba.nikitina@gmail.com>
2022-12-30 13:19:38 +0300

    Fix: del pytest/

--- tests/test_json.py
+++ tests/test_json.py
@@ -5,18 +5,18 @@ import pytest
 
 @pytest.fixture
 def get_json_files():
-    first_file = "pytest/tests/fixtures/file1.json"
-    second_file = "pytest/tests/fixtures/file2.json"
-    result_file = "pytest/tests/fixtures/expected_json.txt"
+    first_file = "tests/fixtures/file1.json"
+    second_file = "tests/fixtures/file2.json"
+    result_file = "tests/fixtures/expected_json.txt"
     one, two, style = get_files_content(test=True, path1=first_file, path2=second_file)
     return one, two, result_file
 
 
 @pytest.fixture
 def get_yaml_files():
-    first_file = "pytest/tests/fixtures/file1.yml"
-    second_file = "pytest/tests/fixtures/file2.yml"
-    result_file = "pytest/tests/fixtures/expected_json.txt"
+    first_file = "tests/fixtures/file1.yml"
+    second_file = "tests/fixtures/file2.yml"
+    result_file = "tests/fixtures/expected_json.txt"
     one, two, _ = get_files_content(test=True, path1=first_file, path2=second_file)
     return one, two, result_file
 
