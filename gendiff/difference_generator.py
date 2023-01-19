8b64f03a5176c91d4e268cb972d951c7e0f233f6
zluuba <zluyba.nikitina@gmail.com>
2023-01-19 10:19:28 +0300

    Fix: split engine into two modules

--- gendiff/difference_generator.py
+++ gendiff/difference_generator.py
@@ -1,41 +1,9 @@
+from gendiff.difference_node import get_diff_node
 from gendiff.formatter import get_format_diff
 from gendiff.loader import upload
 
 
-def get_diff_node(node1, node2, key, walker):
-    diff_node = dict()
-    diff_parts = {'flag': 'default'}
-
-    if key not in node1:
-        diff_parts['flag'] = 'add'
-        diff_parts['new_value'] = node2[key]
-    elif key not in node2:
-        diff_parts['flag'] = 'remove'
-        diff_parts['old_value'] = node1[key]
-    else:
-        value1 = node1[key]
-        value2 = node2[key]
-
-        diff_parts['old_value'] = value1
-        diff_parts['new_value'] = value2
-
-        node1_not_dict = not isinstance(value1, dict)
-        node2_not_dict = not isinstance(value2, dict)
-
-        if value1 != value2 and (node1_not_dict or node2_not_dict):
-            diff_parts['flag'] = 'update'
-
-    if diff_parts['flag'] == 'default':
-        diff_node[key] = walker(
-            diff_parts['old_value'], diff_parts['new_value']
-        )
-    else:
-        diff_node[key] = diff_parts
-
-    return diff_node
-
-
-def generate_diff(file1, file2, style='stylish'):
+def generate_diff(file1, file2, formatter='stylish'):
     uploaded_file1 = upload(file1)
     uploaded_file2 = upload(file2)
 
@@ -53,4 +21,4 @@ def generate_diff(file1, file2, style='stylish'):
         return diff_dict
 
     diff_dictionary = walker(uploaded_file1, uploaded_file2)
-    return get_format_diff(diff_dictionary, style)
+    return get_format_diff(diff_dictionary, formatter)
---
+++ gendiff/difference_node.py
@@ -0,0 +1,31 @@
+def get_diff_node(node1, node2, key, walker):
+    diff_node = dict()
+    diff_parts = {'flag': 'default'}
+
+    if key not in node1:
+        diff_parts['flag'] = 'add'
+        diff_parts['new_value'] = node2[key]
+    elif key not in node2:
+        diff_parts['flag'] = 'remove'
+        diff_parts['old_value'] = node1[key]
+    else:
+        value1 = node1[key]
+        value2 = node2[key]
+
+        diff_parts['old_value'] = value1
+        diff_parts['new_value'] = value2
+
+        node1_not_dict = not isinstance(value1, dict)
+        node2_not_dict = not isinstance(value2, dict)
+
+        if value1 != value2 and (node1_not_dict or node2_not_dict):
+            diff_parts['flag'] = 'update'
+
+    if diff_parts['flag'] == 'default':
+        diff_node[key] = walker(
+            diff_parts['old_value'], diff_parts['new_value']
+        )
+    else:
+        diff_node[key] = diff_parts
+
+    return diff_node
