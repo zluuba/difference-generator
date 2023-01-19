9cebbf89b96981a3cec405c1fe8672af3d6d5489
zluuba <zluyba.nikitina@gmail.com>
2023-01-19 09:25:26 +0300

    Fix: add exception handling

--- gendiff/formatter.py
+++ gendiff/formatter.py
@@ -1,10 +1,12 @@
 from gendiff.formatters import stylish, plain, json
 
 
-def get_format_diff(diff_dict, key='stylish'):
-    if key == 'plain':
+def get_format_diff(diff_dict, format_name):
+    if format_name == 'plain':
         return plain.get_plain_diff(diff_dict)
-    elif key == 'json':
+    elif format_name == 'json':
         return json.get_json_diff(diff_dict)
-
-    return stylish.get_stylish_diff(diff_dict)
+    elif format_name == 'stylish':
+        return stylish.get_stylish_diff(diff_dict)
+    else:
+        raise ValueError(f'Unknown format: {format_name}')
