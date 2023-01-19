b28cdf9b4c6bf40d8e966e6d3d7ca19d1009f724
zluuba <zluyba.nikitina@gmail.com>
2023-01-19 09:26:09 +0300

    Fix: split upload function

--- gendiff/loader.py
+++ gendiff/loader.py
@@ -3,13 +3,18 @@ import yaml
 import os
 
 
-def upload(file):
-    _, file_extension = os.path.splitext(file)
+def load_file(data, file_extension):
     if file_extension == '.json':
-        uploaded_file = json.load(open(file))
+        uploaded_file = json.load(data)
     elif file_extension in {'.yaml', '.yml'}:
-        uploaded_file = yaml.load(open(file), Loader=yaml.Loader)
+        uploaded_file = yaml.load(data, Loader=yaml.Loader)
     else:
         raise ValueError(f'Incorrect format: {file_extension}')
+    return uploaded_file
 
+
+def upload(source_file):
+    _, file_extension = os.path.splitext(source_file)
+    with open(source_file) as data:
+        uploaded_file = load_file(data, file_extension)
     return uploaded_file
